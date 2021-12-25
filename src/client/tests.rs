03dbd882bdd34e92d3034378db5fb934cf94bf49
Taylan Gökkaya <insomnimus.dev@gmail.com>
2021-12-25 20:19:01 +0300

    test: added a unit test for send_file

--- src/client/tests.rs
+++ src/client/tests.rs
@@ -1,3 +1,24 @@
+use std::{
+	fs,
+	io::{
+		self,
+		Cursor,
+		Read,
+		Write,
+	},
+};
+
+use rand::{
+	distributions::Alphanumeric,
+	Fill,
+	Rng,
+};
+use snap::{
+	read::FrameDecoder,
+	write::FrameEncoder,
+};
+use temp_dir::TempDir;
+
 use super::*;
 use crate::{
 	encryption::KeyedXor,
@@ -6,7 +27,6 @@ use crate::{
 
 #[test]
 fn test_send_msg() {
-	use std::io::Read;
 	let tests = &[
 		("password1", "first message; what's up"),
 		("password123", "don't use that password"),
@@ -32,7 +52,7 @@ fn test_send_msg() {
 		);
 
 		// Now we read.
-		let mut dummy_receiver = std::io::Cursor::new(dummy_connection);
+		let mut dummy_receiver = Cursor::new(dummy_connection);
 		// Read the header from the "connection".
 
 		assert_eq!(
@@ -53,5 +73,58 @@ fn test_send_msg() {
 		// Now we decrypt it with our key and assert that it's the message we sent.
 		let decrypted_msg = xor.decrypt(&received_cipher_text);
 		assert_eq!(msg.as_bytes(), &decrypted_msg, "decryption failed",);
+	}
+}
+
+#[test]
+fn test_send_file() {
+	let tmp = TempDir::new().unwrap();
+
+	let tests = &[
+		("demo.txt", "password4321"),
+		("with space.jpeg", "some random password"),
+		("qwertyuıopğ.bin", "asdfasdf"),
+		("notavirus.cmd", "itwasavirus"),
+		("int main", "(**char argv)"),
+	];
+
+	for (file_name, password) in tests {
+		let mut random_data = [0_u8; 2048];
+		random_data
+			.try_fill(&mut rand::thread_rng())
+			.expect("failed to fill buffer with random bytes");
+		let path = tmp.child(file_name);
+		// Write the data to the file.
+		fs::write(&path, &random_data).expect("failed to generate test file");
+		let mut dummy_con = Vec::new();
+		let xor = KeyedXor::new(password);
+
+		// "Send" the file.
+		assert_eq!(Ok(()), send_file(&mut dummy_con, &path, &xor));
+
+		// Read past the header, assert that it's a file.
+		// We don't check for len here since testing the file validity is equivalent.
+		let mut dummy_receiver = Cursor::new(dummy_con);
+		let header: Header = bincode::deserialize_from(&mut dummy_receiver)
+			.expect("received data does not start with a valid header");
+		match header {
+			Header::File {
+				name, compressed, ..
+			} if name.eq(file_name) && compressed => (),
+			x => panic!("header unexpected header: {:?}", x),
+		};
+		// Now read the compressed and encrypted  file.
+		let mut received_data = Vec::new();
+		dummy_receiver.read_to_end(&mut received_data).unwrap();
+		xor.decrypt_in_place(&mut received_data);
+		let mut uncompressed = Vec::new();
+		let mut unarchiver = FrameDecoder::new(Cursor::new(received_data));
+		io::copy(&mut unarchiver, &mut uncompressed).expect("failed to decompress received file");
+
+		assert_eq!(
+			&random_data[..],
+			&uncompressed[..],
+			"received data is not the same as sent data",
+		);
 	}
 }
